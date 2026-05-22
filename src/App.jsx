4bacdd1cad871b9f158e9db81214cd025c135e95
Michael-Nyawade <mikeokello17@gmail.com>
2026-05-23 00:24:11 +0300

    Added About section with professional background narrative

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 import Container from "./components/Container";
 
 function App() {
   return (
     <Container>
       <Hero />
+      <About />
     </Container>
   );
 }
