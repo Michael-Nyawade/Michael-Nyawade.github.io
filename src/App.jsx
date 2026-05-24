c4d7e60a54304cbf931aa19a781ec8ccb69048ed
Michael-Nyawade <mikeokello17@gmail.com>
2026-05-24 11:05:21 +0300

    Integrated Testimonials section into portfolio page structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./sections/About";
 import Experience from "./sections/Experience";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
+import Testimonials from "./sections/Testimonials";
 import Contact from "./sections/Contact";
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
       <Experience />
       <Skills />
       <Projects />
+      <Testimonials />
       <Contact />
     </>
   );
